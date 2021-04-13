8e243ee85dc91e833327bc3af318cef1abf36027
willvelida <willvelida@hotmail.co.uk>
2021-04-13 18:49:12 +1200

    Adding intial function code

---
+++ MyHealth.FileValidator.Sleep/MyHealth.FileValidator.Sleep/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyHealth.Common;
+using MyHealth.FileValidator.Sleep;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace MyHealth.FileValidator.Sleep
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+            builder.Services.AddLogging();
+
+            builder.Services.AddSingleton(sp =>
+            {
+                IConfiguration config = sp.GetService<IConfiguration>();
+                return new ServiceBusHelpers(config["ServiceBusConnectionString"]);
+            });
+
+            builder.Services.AddSingleton(sp =>
+            {
+                IConfiguration config = sp.GetService<IConfiguration>();
+                return new AzureBlobHelpers(config["BlobStorageConnectionString"], config["MyHealthContainer"]);
+            });
+        }
+    }
+}
