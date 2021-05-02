5d60ff32c240495bcb4223d09daff2f30b39e55d
willvelida <willvelida@hotmail.co.uk>
2021-05-02 20:47:11 +1200

    Adding exception check and unit test in parser

---
+++ MyHealth.FileValidator.Sleep/MyHealth.FileValidator.Sleep.UnitTests/ParserTests/SleepRecordParserShould.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using MyHealth.Common;
+using MyHealth.FileValidator.Sleep.Parsers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyHealth.FileValidator.Sleep.UnitTests.ParserTests
+{
+    public class SleepRecordParserShould
+    {
+        private Mock<IConfiguration> _mockConfiguration;
+        private Mock<IServiceBusHelpers> _mockServiceBusHelpers;
+        private Mock<Stream> _mockStream;
+
+        private SleepRecordParser _sut;
+
+        public SleepRecordParserShould()
+        {
+            _mockConfiguration = new Mock<IConfiguration>();
+            _mockServiceBusHelpers = new Mock<IServiceBusHelpers>();
+            _mockStream = new Mock<Stream>();
+
+            _sut = new SleepRecordParser(
+                _mockConfiguration.Object,
+                _mockServiceBusHelpers.Object);
+        }
+
+        [Fact]
+        public async Task ThrowExceptionWhenStreamStartFails()
+        {
+            // Arrange
+            _mockStream.Setup(x => x.Seek(It.IsAny<long>(), It.IsAny<SeekOrigin>())).Throws(new Exception());
+
+            // Act
+            Func<Task> parserAction = async () => await _sut.ParseSleepStream(_mockStream.Object);
+
+            // Assert
+            parserAction.Should().Throw<Exception>();
+
+        }
+    }
+}
--- MyHealth.FileValidator.Sleep/MyHealth.FileValidator.Sleep/Parsers/SleepRecordParser.cs
+++ MyHealth.FileValidator.Sleep/MyHealth.FileValidator.Sleep/Parsers/SleepRecordParser.cs
@@ -24,34 +24,41 @@ namespace MyHealth.FileValidator.Sleep.Parsers
 
         public async Task ParseSleepStream(Stream inputStream)
         {
-            inputStream.Seek(0, SeekOrigin.Begin);
-
-            using (var sleepStream = new StreamReader(inputStream))
-            using (var csv = new CsvReader(sleepStream, CultureInfo.InvariantCulture))
+            try
             {
-                if (csv.Read())
-                {
-                    csv.ReadHeader();
-                    while (csv.Read())
-                    {
-                        var sleep = new mdl.Sleep
-                        {
-                            StartTime = DateTime.ParseExact(csv.GetField("Start Time"), "d/MM/yyyy H:mm", null),
-                            EndTime = DateTime.ParseExact(csv.GetField("End Time"), "d/MM/yyyy H:mm", null),
-                            SleepDate = DateTime.ParseExact(csv.GetField("Start Time"), "d/MM/yyyy H:mm", null).ToString("d/MM/yyyy"),
-                            MinutesAsleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Asleep"))),
-                            MinutesAwake = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Awake"))),
-                            NumberOfAwakenings = int.Parse(ConvertNaNToZero(csv.GetField("Number of Awakenings"))),
-                            TimeInBed = int.Parse(ConvertNaNToZero(csv.GetField("Time in Bed"))),
-                            MinutesREMSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes REM Sleep"))),
-                            MinutesLightSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Light Sleep"))),
-                            MinutesDeepSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Deep Sleep")))
-                        };
+                inputStream.Seek(0, SeekOrigin.Begin);
 
-                        await _serviceBusHelpers.SendMessageToTopic(_configuration["SleepTopic"], sleep);
+                using (var sleepStream = new StreamReader(inputStream))
+                using (var csv = new CsvReader(sleepStream, CultureInfo.InvariantCulture))
+                {
+                    if (csv.Read())
+                    {
+                        csv.ReadHeader();
+                        while (csv.Read())
+                        {
+                            var sleep = new mdl.Sleep
+                            {
+                                StartTime = DateTime.ParseExact(csv.GetField("Start Time"), "d/MM/yyyy H:mm", null),
+                                EndTime = DateTime.ParseExact(csv.GetField("End Time"), "d/MM/yyyy H:mm", null),
+                                SleepDate = DateTime.ParseExact(csv.GetField("Start Time"), "d/MM/yyyy H:mm", null).ToString("d/MM/yyyy"),
+                                MinutesAsleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Asleep"))),
+                                MinutesAwake = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Awake"))),
+                                NumberOfAwakenings = int.Parse(ConvertNaNToZero(csv.GetField("Number of Awakenings"))),
+                                TimeInBed = int.Parse(ConvertNaNToZero(csv.GetField("Time in Bed"))),
+                                MinutesREMSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes REM Sleep"))),
+                                MinutesLightSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Light Sleep"))),
+                                MinutesDeepSleep = int.Parse(ConvertNaNToZero(csv.GetField("Minutes Deep Sleep")))
+                            };
+
+                            await _serviceBusHelpers.SendMessageToTopic(_configuration["SleepTopic"], sleep);
+                        }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                throw ex;
+            }          
         }
 
         private string ConvertNaNToZero(string csvField)
